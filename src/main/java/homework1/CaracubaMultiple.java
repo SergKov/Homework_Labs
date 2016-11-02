3bbc239deaec30af69e6946b1888b99348e5cdaa
Sergey <kovalchyks@gmail.com>
2016-11-02 11:50:10 +0200

    corrected CaracubaMultiple

--- src/main/java/homework1/CaracubaMultiple.java
+++ src/main/java/homework1/CaracubaMultiple.java
@@ -1,40 +1,34 @@
 package homework1;
 
+import java.math.BigInteger;
+
+import static homework1.Utils.*;
+
 /**
  * Created by koval on 30.10.2016.
  */
 public class CaracubaMultiple {
 
-    public long getMultiple(final long first, final long second) {
-        final int firstNumberFirstPart = getFirstPart(first);
-        final long firstNumberSecondPart = first - getFirstAdd(first);
+    public static BigInteger getMultipleRec(final BigInteger first, final BigInteger second) {
 
-        final int secondNumberFirstPart = getFirstPart(second);
-        final long secondNumberSecondPart = second - getFirstAdd(second);
+        int max = Math.max(first.bitLength(), second.bitLength());
 
-        return firstNumberFirstPart * secondNumberFirstPart * getFirstAdd(first) * getFirstAdd(second)
-                + ((getFirstAdd(first) + firstNumberSecondPart) * (getFirstAdd(second) + secondNumberSecondPart))
-                - (getFirstPart(first) * getFirstPart(second) - firstNumberFirstPart * firstNumberSecondPart
-                - firstNumberSecondPart * secondNumberSecondPart);
-
-    }
-
-    private int getFirstPart(final long number) {
-        return (int) (number - getFirstAdd(number));
-    }
-
-    private long getFirstAdd(final long number) {
-        return (long) Math.pow(10, getPower(number));
-    }
-
-    private int getPower(long number) {
-        int power = 0;
-        while (number < 10) {
-            power++;
-            number /= 10;
+        if (!isIntMoreZero(addInts(max, -2000))) {
+            return first.multiply(second);
         }
-        return power;
+
+        max = addInts(max >> 1, max % 2);
+
+        final BigInteger b = first.shiftRight(max);
+        final BigInteger a = first.subtract(b.shiftLeft(max));
+        final BigInteger d = second.shiftRight(max);
+        final BigInteger c = second.subtract(d.shiftLeft(max));
+
+        final BigInteger ac = getMultipleRec(a, c);
+        final BigInteger bd = getMultipleRec(b, d);
+
+        final BigInteger abcd = getMultipleRec(a.add(b), c.add(d));
+
+        return ac.add(abcd.subtract(ac).subtract(bd).shiftLeft(max)).add(bd.shiftLeft(max >> 1));
     }
-
-
 }
